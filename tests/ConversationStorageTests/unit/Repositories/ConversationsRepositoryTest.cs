29ef98bd1f3c76616483ad83d450f661ebac809f
IAALvs <isaacalf.alvs@gmail.com>
2024-03-17 15:30:40 -0600

    fix:_repo di

--- tests/ConversationStorageTests/unit/Repositories/ConversationsRepositoryTest.cs
+++ tests/ConversationStorageTests/unit/Repositories/ConversationsRepositoryTest.cs
@@ -5,7 +5,7 @@ using ConversationStorage.Models;
 using EphemeralMongo;
 using Microsoft.Extensions.Configuration;
 
-public class ConversationRepositoryTest : IClassFixture<ConversationRepositoryConfiguration>
+public class ConversationRepositoryTest : IDisposable
 {
     private readonly IConversationRepository? _repository;
     private IMongoRunner? mongoDbRunner;
@@ -13,11 +13,42 @@ public class ConversationRepositoryTest : IClassFixture<ConversationRepositoryCo
     public IConversationRepository? repository;
     private IConfiguration? _configuration;
 
-    public ConversationRepositoryTest(ConversationRepositoryConfiguration config)
+    public ConversationRepositoryTest()
     {
-        _repository = config.repository;
         Console.WriteLine("REPOSITORY CTO===================================");
         Console.WriteLine(_repository);
+                _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .AddEnvironmentVariables()
+            .Build();
+        string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");      
+        /* TESTING WITH OUT LOCAL DATABASE TO AVOID VERSIONING OF DEPENDENCIES */
+        if(environment == "ci-cd"){
+            var options = new MongoRunnerOptions
+            {
+                UseSingleNodeReplicaSet = true,
+                ConnectionTimeout = TimeSpan.FromSeconds(60),
+                AdditionalArguments = "--quiet",
+                MongoPort = 27017,
+                KillMongoProcessesWhenCurrentProcessExits = true
+            };
+            mongoDbRunner = MongoRunner.Run(options);
+            _mongoClient = new MongoClient(mongoDbRunner.ConnectionString);
+            return;
+        }
+            Console.WriteLine(_configuration.GetConnectionString("MONGO_DB_NAME"));
+            _mongoClient = new MongoClient(_configuration.GetConnectionString("MONGO"));
+            repository = new MongoConversationRepository(_mongoClient, _configuration);
+            return;   
+    }
+    public void Dispose()
+    {
+        var mongoDbEnv = _configuration!["MONGO_DB_ENV"];
+        if(mongoDbEnv == "ci-cd"){
+        _mongoClient!.DropDatabase(_configuration.GetConnectionString("MONGO_DB_NAME"));
+        _mongoClient = null;
+        mongoDbRunner!.Dispose();
+        }
     }
 
     [Fact]
@@ -36,7 +67,7 @@ public class ConversationRepositoryTest : IClassFixture<ConversationRepositoryCo
         Assert.IsType<Conversation>(clientConversation);
     }
 }
-public class ConversationRepositoryConfiguration : IDisposable
+/* public class ConversationRepositoryConfiguration : IDisposable
 {
     private IMongoRunner? mongoDbRunner;
     public IMongoClient? _mongoClient;
@@ -49,7 +80,6 @@ public class ConversationRepositoryConfiguration : IDisposable
             .AddEnvironmentVariables()
             .Build();
         string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");      
-        /* TESTING WITH OUT LOCAL DATABASE TO AVOID VERSIONING OF DEPENDENCIES */
         if(environment == "ci-cd"){
             var options = new MongoRunnerOptions
             {
@@ -81,4 +111,4 @@ public class ConversationRepositoryConfiguration : IDisposable
         mongoDbRunner!.Dispose();
         }
     }
-}
+} */
